6238d0e5deb3d67c912cec117b55b55d148976ea
Taurus <offcltntwi@gmail.com>
2022-04-24 03:42:42 +0800

    Function
    Nav, Seat, Bus

--- mp2022_group6/app/src/main/java/com/example/mp2022_group6/NavigationActivity.java
+++ mp2022_group6/app/src/main/java/com/example/mp2022_group6/NavigationActivity.java
@@ -9,12 +9,21 @@ import android.widget.Button;
 
 public class NavigationActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_navigation);
+
+        Button userProfileButton = findViewById(R.id.userProfileButton);
+        userProfileButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
+            startActivity(intent);
+        });
+
         Button search_buses = findViewById(R.id.search_buses);
         search_buses.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), BusActivity.class);
