8accaeb8add43556e40b099a34ce22c2bee5ddb7
Taurus <offcltntwi@gmail.com>
2022-04-24 08:07:14 +0800

    Update All Java Class
    New
    - Finish Activity
    - Credit Activity

--- mp2022_group6/app/src/main/java/com/example/mp2022_group6/NavigationActivity.java
+++ mp2022_group6/app/src/main/java/com/example/mp2022_group6/NavigationActivity.java
@@ -3,13 +3,16 @@ package com.example.mp2022_group6;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class NavigationActivity extends AppCompatActivity {
-
-
+public class NavigationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,9 @@ public class NavigationActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_navigation);
+
+        Spinner spinner1 = findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(this);
 
         Button userProfileButton = findViewById(R.id.userProfileButton);
         userProfileButton.setOnClickListener(view -> {
@@ -29,5 +35,16 @@ public class NavigationActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), BusActivity.class);
             startActivity(intent);
         });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
